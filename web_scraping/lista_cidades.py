dc828276f79631465778031f713d2ed463f71790
André da Rocha Souza <58944953+andrerochasouza@users.noreply.github.com>
2022-06-07 10:50:23 -0300

    att lista cidades

--- web_scraping/lista_cidades.py
+++ web_scraping/lista_cidades.py
@@ -11,3 +11,15 @@ def lista_cidade_principais():
         principais_cidade[-1][-1] = principais_cidade[-1][-1].strip()
 
     return principais_cidade
+
+def lista_climas_principais():
+    html_clima = req.get('https://www.climatempo.com.br/previsao-do-tempo?page=HOJE')
+    html_clima_soup = bs(html_clima.text, 'html.parser')
+
+    principais_clima = []
+    for clima in html_clima_soup.find_all('div', {'class': 'featured-city-card-temp'}):
+        principais_clima.append(clima.find('span').text)
+
+    return principais_clima
+
+print(lista_climas_principais())

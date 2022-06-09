84d90c8d68acdb3d22da754f3b598cc69b191884
André da Rocha Souza <58944953+andrerochasouza@users.noreply.github.com>
2022-06-09 16:20:46 -0300

    testando novas ferramentas

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen, urlretrieve, Request
+from urllib.error import HTTPError, URLError
+import pandas as pd
+
+# # pegar as trends do twitter
+# url = 'https://twitter.com/i/trends'
+# twitter = req.get(url)
+# twitter_soup = bs(twitter.text, 'html.parser')
+# print(twitter_soup)
+
+# main_page_twitter = twitter_soup.find('main', {'role': 'main'})
+# print(main_page_twitter)
+
+# trends = []
+
+# print(trends)
 
